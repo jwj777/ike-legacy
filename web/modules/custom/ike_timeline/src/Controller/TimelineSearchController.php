c4b5eec48f4e5ad400f4bdb8acbfb4a76dfefa2e
mani <mani@symmdes.com>
2024-06-24 15:01:15 -0700

    timeline module update

--- web/modules/custom/ike_timeline/src/Controller/TimelineSearchController.php
+++ web/modules/custom/ike_timeline/src/Controller/TimelineSearchController.php
@@ -4,36 +4,45 @@ namespace Drupal\ike_timeline\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * The controller for searching the Timeline.
+ */
 class TimelineSearchController extends ControllerBase {
 
+  /**
+   * The search command that returns the results.
+   */
   public function search() {
     $result = [];
 
     $term = $_GET['term'];
-    $json = json_decode(file_get_contents($this->siteURL() . '/modules/custom/ike_timeline/src/static/api/milestones.json'), true);
+    $json = json_decode(file_get_contents($this->siteUrl() . '/modules/custom/ike_timeline/src/static/api/milestones.json'), TRUE);
 
     foreach ($json['data'] as $milestone) {
       $title = $milestone['title'];
 
-      if(preg_match("/{$term}/i", $title)) {
+      if (preg_match("/{$term}/i", $title)) {
         $result[] = [
           'collection' => 'milestones',
           'oid' => $milestone['oid'],
-          'title' => $title
+          'title' => $title,
         ];
       }
     }
 
-    $results_container = array('results' => $result);
+    $results_container = ['results' => $result];
     return new JsonResponse($results_container);
   }
 
-  public function siteURL() {
+  /**
+   * Get's the current site URL derived from environment variables.
+   */
+  public function siteUrl() {
     $protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') ||
       $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
     $domainName = $_SERVER['HTTP_HOST'];
-    return $protocol.$domainName;
+    return $protocol . $domainName;
   }
+
 }
